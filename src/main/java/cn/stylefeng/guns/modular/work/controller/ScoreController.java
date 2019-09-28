bed2e4107bf5e44f0b3cbabcb8fe08891740884c
liangsj-cc <lsj_wonderful@126.com>
2019-09-28 18:13:01 +0800

    分数控制器

---
+++ src/main/java/cn/stylefeng/guns/modular/work/controller/ScoreController.java
@@ -0,0 +1,28 @@
+package cn.stylefeng.guns.modular.work.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 人员控制器
+ *
+ * @author liangsj
+ * @Date 2019年9月28日20:27:22
+ */
+@Controller
+@RequestMapping("/people")
+public class ScoreController {
+
+    private String PREFIX = "/modular/work/people/";
+
+    /**
+     * 跳转到人员管理首页
+     *
+     * @author fengshuonan
+     * @Date 2018/12/23 4:56 PM
+     */
+    @RequestMapping("")
+    public String index() {
+        return PREFIX + "people.html";
+    }
+}
